ebe74e85b4e7cd4cf56996e573e6378cfed2eea8
danieltunon <danieltunon@gmail.com>
2016-03-02 15:48:49 -0800

    Implement functional pattern queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var startVal = 1;
+  var numItems = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    numItems++;
+    storage[startVal + numItems - 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var val = storage[startVal];
+
+    startVal++;
+
+    numItems = Math.max(--numItems, 0);
+
+    return val;
   };
 
   someInstance.size = function() {
+    return numItems;
   };
 
   return someInstance;
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.numItems = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.size = function() {
+  return this.numItems;
+};
+
+Stack.prototype.push = function(item) {
+  this.numItems++;
+  this.storage[this.numItems] = item;
+};
+
+Stack.prototype.pop = function() {
+  if ( this.numItems ) return this.storage[this.numItems--];
+};
 
