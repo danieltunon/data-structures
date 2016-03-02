e729334c881bb89e2b7fb2e482f4dab196b4a27e
Travis Baratcart <baratcart.travis@gmail.com>
2016-03-02 12:18:27 -0800

    Make stack shared behavior tests pass.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numItems = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    numItems++;
+    storage[numItems] = value;
   };
 
   someInstance.pop = function() {
+    if (numItems > 0) {
+      var retVal = storage[numItems];
+      numItems = Math.max(--numItems, 0);
+      return retVal;
+    }
+
   };
 
   someInstance.size = function() {
+    return numItems;
   };
 
   return someInstance;
