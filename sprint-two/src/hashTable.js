6a2d38d310f4c07f7fc740eaeae3ae492960dccd
danieltunon <danieltunon@gmail.com>
2016-03-04 12:21:58 -0800

    Implement hash table resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,10 +6,16 @@ var HashTable = function() {
   this.numItems = 0;
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function(k, v, suppressCounter) {
+  /* suppressCounter is an optional argument used for resize */
+  /* it prevents incremementing numitems when migrating data */
+  this.resize();
+
+  if (!suppressCounter) this.numItems++;
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   var arr = this._storage.get(index);
-  
+
   if (arr) {
     this.remove(k);
   } else {
@@ -18,13 +24,13 @@ HashTable.prototype.insert = function(k, v) {
 
   arr.push([k, v]);
   this._storage.set(index, arr);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var searchArr = this._storage.get(index);
   if (searchArr.length > 0) {
-    // return this._storage.get(index)[k];
     for (var i = 0; i < searchArr.length; i++) {
       if (searchArr[i][0] === k) return searchArr[i][1];
     }
@@ -36,20 +42,46 @@ HashTable.prototype.remove = function(k) {
   var searchArr = this._storage.get(index);
 
   for (var i = 0; i < searchArr.length; i++) {
-    if (searchArr[i][0] === k) searchArr.splice(i, 1);
+    if (searchArr[i][0] === k) {
+      searchArr.splice(i, 1);
+      this.numItems--;
+    }
   }
 
-  // if (Object.keys(obj).length === 1) {
-  //   this._storage.set(index, undefined);
-  // } else {
-  //   delete obj[k];
-  // }
+  this.resize();
 };
 
+HashTable.prototype.resize = function () {
+  var oldLimit = this._limit;
+  var newLimit = oldLimit;
+
+  if (this.numItems === 0) return undefined;
+
+  if (this.numItems / oldLimit > 0.75) {
+    newLimit = oldLimit * 2;
+  } else if (this.numItems / oldLimit < 0.25) {
+    newLimit = Math.max(oldLimit / 2, 8);
+  }
+
+  this._limit = newLimit;
+
+  if (newLimit !== oldLimit) {
+    debugger;
+    var oldData = this._storage;
+    this._storage = LimitedArray(newLimit);
+
+    for (var i = 0; i < oldLimit; i++) {
+      var tuples = oldData.get(i);
+      if (Array.isArray(tuples)) {
+        for (var j = 0; j < tuples.length; j++) {
+          this.insert(tuples[j][0], tuples[j][1], true);
+        }
+      }
+    }
+  }
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
