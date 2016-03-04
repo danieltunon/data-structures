7eab9d19c36f2bbfab102555edca399db9eea7d8
danieltunon <danieltunon@gmail.com>
2016-03-03 16:24:04 -0800

    Implement binary searck tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,41 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  tree.insert = function(value) {
+    if (value < this.value) {
+      this.left === null ? this.left = BinarySearchTree(value) : this.left.insert(value);
+    } else {
+      this.right === null ? this.right = BinarySearchTree(value) : this.right.insert(value);
+    }
+  };
+
+  tree.contains = function(value) {
+    if (this.value === value) {
+      return true;
+    } else if (value < this.value && this.left !== null) {
+      return this.left.contains(value);
+    } else if (this.right !== null){
+      return this.right.contains(value);
+    }
+
+    return false;
+  };
+
+  tree.depthFirstLog = function(cb) {
+    cb(this.value);
+    if (this.left !== null) {
+      this.left.depthFirstLog(cb);
+    }
+    if (this.right !== null) {
+      this.right.depthFirstLog(cb);
+    }
+  };
+
+  return tree;
 };
 
 
